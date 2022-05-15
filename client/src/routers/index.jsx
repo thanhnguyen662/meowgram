0312756aa3e2b8ac98664237067ccdc0979cc935
thanhnguyen662 <facebook662311@gmail.com>
2022-05-15 22:24:13 +0700

    feat: 🎸 private route && profile page

--- client/src/routers/index.jsx
+++ client/src/routers/index.jsx
@@ -4,19 +4,22 @@ import Auth from '../features/Auth';
 import Home from '../features/Home';
 import Loader from '../layouts/Loader';
 import Main from '../layouts/Main';
+import Profile from '../features/Profile';
+import Private from '../layouts/Private';
+import NotFoundPage from '../global/pages/NotFoundPage';
 
 const Routers = () => {
    return (
-      // <Routes>
-      //    <Route path='/auth/*' element={<Auth />} />
-      //    <Route path='/*' element={<Home />} />
-      // </Routes>
       <Loader>
          <Routes>
             <Route path='/auth/*' element={<Auth />} />
             <Route element={<Main />}>
                <Route path='/home/*' element={<Home />} />
             </Route>
+            <Route element={<Private />}>
+               <Route path='/profile/*' element={<Profile />} />
+            </Route>
+            <Route path='*' element={<NotFoundPage />} />
          </Routes>
       </Loader>
    );
