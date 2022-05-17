bf3494a910f8943952af8c27ee91a347ca722a07
thanhnguyen662 <facebook662311@gmail.com>
2022-05-17 20:14:47 +0700

    feat: 🎸 header bar responsive

--- client/src/global/components/Header/index.jsx
+++ client/src/global/components/Header/index.jsx
@@ -1,11 +1,28 @@
-import { Center, Container, Grid, GridItem } from '@chakra-ui/react';
+import {
+   Button,
+   Center,
+   Container,
+   Flex,
+   Grid,
+   GridItem,
+   HStack,
+   IconButton,
+   Menu,
+   MenuButton,
+   MenuItem,
+   MenuList,
+   useBreakpointValue,
+} from '@chakra-ui/react';
 import React from 'react';
+import { BsList } from 'react-icons/bs';
 import { useNavigate } from 'react-router-dom';
 import { TextLogo } from '../Logo';
+import MeowAvatar from '../MeowAvatar';
 import Search from '../Search';
 
 const Header = () => {
    const navigate = useNavigate();
+   const breakpoint = useBreakpointValue({ base: 'mobile', md: 'desktop' });
 
    const onClickLogo = () => {
       navigate('/home');
@@ -26,11 +43,32 @@ const Header = () => {
                </GridItem>
 
                <GridItem colSpan={{ base: 20, md: 11 }}>
-                  <Search />
+                  {breakpoint === 'desktop' ? <Search /> : <HeaderMobile />}
                </GridItem>
             </Grid>
          </Container>
       </Center>
+   );
+};
+
+const HeaderMobile = () => {
+   return (
+      <Flex gap='10px'>
+         <Search />
+         <Menu>
+            <MenuButton>
+               <MeowAvatar
+                  outline={true}
+                  width='41px'
+                  height='41px'
+                  src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9rJMMEpWQ0NPwk7qTq3Zmug9Rmmm2iREdVg&usqp=CAU'
+               />
+            </MenuButton>
+            <MenuList>
+               <MenuItem>New Tab</MenuItem>
+            </MenuList>
+         </Menu>
+      </Flex>
    );
 };
 
